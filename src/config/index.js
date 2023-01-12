94f8147647fc4712a662554b84e462460114f638
Gabriel Maiori <gabrielmaiori@gmail.com>
2023-01-12 00:46:40 -0300

    agrego estructurita inicial de api

--- src/config/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = {
-    mongo: {
-        uri: process.env.MONGO_URI || "mongodb://localhost:27017/test"
-    },
-    port: process.env.PORT || 9042
-};
